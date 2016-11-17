a2eb967b4bc57a84d7500abddc5a6408fbfe976a
malikbenabda <malik.benabda@gmail.com>
2016-11-17 17:21:53 +0100

    recovery commit
    commit from local storage

---
+++ LinuxHelperEsprit/quizz.js
@@ -0,0 +1,14 @@
+
+
+    document.getElementById("first").innerHTML = "whatever";
+
+                        var x = document.getElementById("changer");
+                        x.setAttribute("src", "styles/clickMe01.png");
+
+
+
+
+
+
+
+
