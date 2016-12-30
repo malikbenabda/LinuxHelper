26e9251ee40badfd60fcd340d0cdd2141706fdc9
Malik Benabda <malik.benabda@gmail.com>
2016-12-30 19:54:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php header("Location: LinuxHelperEsprit/index.html"); /* Redirect browser */
 exit() ;
 ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1;url=./LinuxHelperEsprit/index.html">
+        <script type="text/javascript">
+            window.location.href = "./LinuxHelperEsprit/index.html"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow the <a href='https://malikbenabda.github.io/LinuxHelper/LinuxHelperEsprit/index.html'>link to example</a>
+    </body>
+</html>
